19cd7f96e7511855021a6ac2d3a89f829485fcce
Nevin <39220032+TypicalNevin@users.noreply.github.com>
2020-06-16 00:10:07 +0200

    finished the project

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ var path = require("path");
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 var chalk = require("chalk");
+const getSize = require("get-folder-size");
+const getFolderSize = require("get-folder-size");
 var port = process.env.PORT || 3000;
+
+var folderStats = {
+  numPastes: 0,
+  size: 0,
+};
 
 server.listen(port, () => {
   console.log("server listening on port 3000");
@@ -16,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 fs.stat(__dirname + "/pastes/", (err) => {
   if (err)
     fs.mkdir(__dirname + "/pastes", (err) => {
-      if (err) throw err;
+      if (err) console.log(chalk.red("Unable to create directory"));
       console.log(chalk.yellow("pastes directory didn't exist. Created one"));
     });
 });
@@ -34,12 +41,14 @@ io.on("connection", (socket) => {
       socket.emit("server reject file: over 10k chars");
       console.log(chalk.red("File was rejected: Over 10k chars"));
     } else {
+      var randomString = generateRandomString(8);
       fs.writeFile(
-        __dirname + "/pastes/" + generateRandomString(8) + ".txt",
+        __dirname + "/pastes/" + randomString + ".txt",
         fileContext,
         (err) => {
-          if (err) throw err;
+          if (err) if (err) console.log(chalk.red("Unable to write to file"));
           console.log(chalk.green("Paste was successfully created"));
+          socket.emit("paste successfully created", randomString);
         }
       );
     }
@@ -70,5 +79,30 @@ io.on("connection", (socket) => {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
-  }; // make sure its UTF-8
+  };
+
+  const getFolderStats = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Got info");
+      }, 100);
+      fs.readdir(__dirname + "/pastes", (err, files) => {
+        folderStats["numPastes"] = files.length;
+        console.log(files.length);
+      });
+      getFolderSize(__dirname + "/pastes", (err, size) => {
+        if (err) console.log(chalk.red("Unable to get directory size"));
+        folderStats["size"] = size;
+        console.log(size);
+      });
+    });
+  };
+
+  const sendFolderStats = () => {
+    socket.emit("server send folder stats", folderStats);
+  };
+
+  getFolderStats()
+    .then(sendFolderStats)
+    .catch((err) => console.log(chalk.red(err)));
 });
--- public/main.js
+++ public/main.js
@@ -1,15 +1,24 @@
 $(function () {
   var socket = io();
 
+  var pasteStatus = false;
+
   var $createPaste = document.getElementById("createPasteButton");
   var $userInput = document.getElementById("userInput");
+  var $footer = document.getElementById("footer");
   var $urlString = window.location.href;
+  var $currentURL =
+    location.protocol + "//" + location.host + location.pathname;
 
   $userInput.value = "";
 
   var url = new URL($urlString);
   var params = url.searchParams.get("paste");
-  socket.emit("client request paste", params);
+  if (params !== null) {
+    pasteStatus = true;
+    socket.emit("client request paste", params);
+    $createPaste.innerText = "Check the URL bar for the link";
+  }
 
   $createPaste.onclick = () => {
     socket.emit("client send new paste", $userInput.value);
@@ -17,5 +26,13 @@ $(function () {
 
   socket.on("server send paste data", (data) => {
     $userInput.value = data;
+  });
+
+  socket.on("paste successfully created", (randomString) => {
+    window.location.href = $currentURL + "?paste=" + randomString;
+  });
+
+  socket.on("server send folder stats", (folderData) => {
+    $footer.innerText = `Currently Storing ${folderData["numPastes"]} Pastes, and taking up ${folderData["size"]} Bytes`;
   });
 });
